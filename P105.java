6a7138d852ce7c2f7010e0e4b8b405cfe5a5436f
Sourav Deb <souravdeb1240@gmail.com>
2025-09-06 12:32:14 +0600

    Add files via upload

---
+++ P105.java
@@ -0,0 +1,6 @@
+public class P105 {
+    public static void main(String[] args) {
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+    System.out.println(myNumbers[1][2]);
+  }
+}
---
+++ P110.java
@@ -0,0 +1,11 @@
+public class P110 {
+    public static void main(String[] args) {
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+
+    for (int[] row : myNumbers) {
+      for (int num : row) {
+        System.out.println(num);
+      }
+    }
+  }
+}
